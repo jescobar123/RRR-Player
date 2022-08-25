c9a5f74eab0e5a71f983ac1e192d764d222724ac
Juan Diego Escobar <jdescobar@iemsantateresita.edu.co>
2022-08-25 13:12:24 -0500

    Inicial commit

---
+++ app/src/main/java/com/stasoft/rrrplayer/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.stasoft.rrrplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    // añadir cometario
+}
