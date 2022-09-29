e9a05c47fc0c39c89af214ce4802e4baaf096b0e
Juan Diego Escobar <jdescobar@iemsantateresita.edu.co>
2022-09-29 11:59:54 -0500

    Inicial commit

--- app/src/main/java/com/stasoft/rrrplayer/InicioActivity.kt
+++ app/src/main/java/com/stasoft/rrrplayer/InicioActivity.kt
@@ -1,11 +1,31 @@
 package com.stasoft.rrrplayer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import com.stasoft.rrrplayer.PrincipalActivity
 
 class InicioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
+
+        startTimer()
+    }
+    fun startTimer(){
+        object: CountDownTimer(3000,1000){
+            override fun onTick(p0: Long) {
+
+
+            }
+
+            override fun onFinish() {
+                val intent = Intent(applicationContext, PrincipalActivity::class.java).apply{}
+                startActivity(intent)
+
+            }
+
+        }.start()
     }
 }
---
+++ app/src/main/java/com/stasoft/rrrplayer/PrincipalActivity.kt
@@ -0,0 +1,16 @@
+package com.stasoft.rrrplayer
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import kotlin.system.measureTimeMillis
+
+class PrincipalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+    }
+
+
+}
